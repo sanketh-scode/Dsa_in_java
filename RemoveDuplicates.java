48c1c37494176484db62cf478284feb712a5c93a
sanketh <sanketh.study5@gamil.com>
2026-03-12 21:40:27 +0530

    String builder

---
+++ RemoveDuplicates.java
@@ -0,0 +1,19 @@
+public class RemoveDuplicates {
+    static void removeDuplicates(String s){
+        StringBuilder sb= new StringBuilder();
+        boolean []visited=new boolean[];
+        
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(!visited[ch]){
+                sb.append(ch);
+                visited[ch]=true;
+            }
+        }
+        System.out.println(sb);
+    }
+    public static void main(String[] args) {
+        String s="programminglanguage";
+        removeDuplicates(s);
+    }    
+}
